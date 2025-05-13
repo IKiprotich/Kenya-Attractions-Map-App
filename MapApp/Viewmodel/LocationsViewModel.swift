b8659cf644ec92188213e9740105bd4cf61613e0
Ian Kiprotich <kiprotichian254@gmail.com>
2025-05-13 12:48:32 +0300

    feat: added a map using mapkit

--- MapApp/Viewmodel/LocationsViewModel.swift
+++ MapApp/Viewmodel/LocationsViewModel.swift
@@ -6,12 +6,36 @@
 //
 
 import Foundation
+import MapKit
+import SwiftUI
 
 class LocationsViewModel: ObservableObject {
+    
+    //All loaded locations
     @Published var locations: [Location]
+    
+    //Curent location on the map
+    @Published var mapLoacation: Location{
+        didSet{
+            updateMapRegion(location: mapLoacation)
+        }
+    }
+    
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
+        self.mapLoacation = locations.first!
+        self.updateMapRegion(location: locations.first!)
+    }
+    
+    private func updateMapRegion(location: Location) {
+        withAnimation(.easeInOut){
+            mapRegion = MKCoordinateRegion(center: location.coordinates, span: mapSpan)
+            
+        }
     }
 }
